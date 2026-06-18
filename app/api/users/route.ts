0661ed619a0ca156cf333fb0b50e13cb19c28740
bellyea <bellyeahlle@gmail.com>
2026-06-18 22:37:51 +0900

    feat: setup prisma and user model

---
+++ app/api/users/route.ts
@@ -0,0 +1,21 @@
+import { prisma } from "@/lib/prisma";
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  const users = await prisma.user.findMany();
+
+  return NextResponse.json(users);
+}
+
+export async function POST(request: Request) {
+  const body = await request.json();
+
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      nickname: body.nickname,
+    },
+  });
+
+  return NextResponse.json(user);
+}
