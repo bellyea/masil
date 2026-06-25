6668a6d6329e0b0207de42483a8bf04f4fed31a7
bellyea <bellyeahlle@gmail.com>
2026-06-26 06:56:25 +0900

    chore: add masil logo assets

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Masil | 문화생활, 마실 가듯 가볍게",
   description: "전시와 공연을 발견하고 북마크하며 AI에게 추천받는 문화 이벤트 탐색 서비스",
+  icons: {
+    icon: "/icon.png",
+    apple: "/apple-icon.png",
+  },
 };
 
 export default function RootLayout({
